599e3e5ac5cd944b9299b286ef0dac88aed0e5e7
Raihan Rifandi <raihanrifandi73@gmail.com>
2024-12-19 08:13:39 +0700

    template dashboard

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,7 +3,7 @@
 <div x-cloak :class="sidebarOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'" class="fixed inset-y-0 left-0 z-0 w-64 overflow-y-auto transition duration-300 transform bg-[#1d4ed8] lg:translate-x-0 lg:static lg:inset-0">
     <div class="flex items-center justify-left mt-8 px-6">
         <div class="flex items-center">
-            <img src="assets/logo.png" class="w-12 h-12" alt="Logo Ayam">
+            {{-- <img src="assets/logo.png" class="w-12 h-12" alt="Logo Ayam"> --}}
             <span class="mx-2 text-[16px] font-bold text-white">CHICKCARE</span>
         </div>
     </div>
@@ -27,7 +27,7 @@
 
       <a class="flex items-center px-6 py-2 mt-4 text-white hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100 {{ Route::is('admin.dashboard') ? 'bg-gray-700 bg-opacity-25' : '' }}" href="{{ route('admin.dashboard') }}">
         <i class="fa-solid fa-location-dot w-6 h-6"></i>
-      <span class="mx-3">Knowledge Base</span>
+      <span class="mx-3">Basis Pengetahuan</span>
     </a>
 
         <a class="flex items-center px-6 py-2 mt-4 text-white hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100" href="{{ route('logout') }}" onclick="confirmLogout(event)"; 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>HALAMAN ADMIN</title>
+        <title>Admin</title>
 
         <!-- Scripts -->
-        @vite('resources/sass/app.scss', 'resources/js/app.js') 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
